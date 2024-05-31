773001cfea47c15cf4d12830e1da5392db972a18
Catalin <stirbuc@icloud.com>
2024-05-31 17:48:48 +0300

    New File:     Algorithms:         Bubble Sort.py:             Made it a function, added treating errors and an alternative to a line of code.

--- Algorithms/Bubble Sort.py
+++ Algorithms/Bubble Sort.py
@@ -1,9 +1,36 @@
-a = [5, 7, 2, 4, 1, 9]
-N = len(a)
+N = input("Please type the length of the list: ")
+try:
+    N = int(N)
+except ValueError:
+    print("Please type a valid number!")
+a = []
+try:
+    for x in range(0, N):
+        y = int(input("Please type a number to be added to the list: "))
+        a.append(y)
+except ValueError:
+    print("Please type a valid number!")
+except TypeError:
+    print("Please type a valid number!")
 
-for i in range(1, N):
-    for j in range(0, N - 1):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
-print(a)
-# TO DO: WRITE THE PROGRAM IN A FUNCTION AND MAKE THE USER WRITE THE LIST BY INPUT AND NO EXCEPTIONS (TRY AND EXCEPT)
+
+def bubble_sort(list1):
+    """Sort a list in ascending order using the bubble sort algorithm.
+    Args:
+        list1: array/list to be sorted
+    Returns:
+        The sorted list
+    """
+    try:
+        for i in range(1, N):
+            for j in range(0, N - 1):
+                if list1[j] > list1[j + 1]:
+                    list1[j], list1[j + 1] = list1[j + 1], list1[j]
+        return list1
+    except TypeError:
+        print("Please type a valid number!")
+    except ValueError:
+        print("Please type a valid number!")
+
+
+print(bubble_sort(a))
