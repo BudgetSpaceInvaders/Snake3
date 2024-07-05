68415dd5b20546917e4a7ce1c9d0d3abaa024678
Catalin <stirbuc@icloud.com>
2024-07-05 17:34:09 +0300

    New File:     Algorithms:         Merge Sort.py:             Sorts the list by making smaller lists and them uniting them.

---
+++ Algorithms/Merge Sort.py
@@ -0,0 +1,59 @@
+def get_list_from_user():
+    """Giving the user the ability to make his own list.
+            n: length
+        Returns:
+            a: list of integers obtained from user input."""
+
+    while True:
+        try:
+            n = int(input("Please type the length of the list: "))
+            a = [int(input("Please type a number: ")) for _ in range(n)]
+            return a
+        except ValueError:
+            print("Please type a valid number!")
+
+
+def mergesort(a):
+    n = len(a)
+    mid = n // 2
+    if n <= 1:
+        return a
+
+    left = a[:mid]
+    right = a[mid:]
+
+    ar1 = mergesort(left)
+    ar2 = mergesort(right)
+
+    return merge(ar1, ar2)
+
+
+def merge(ar1, ar2):
+    ar3 = []
+    while ar1 and ar2:
+        if ar1[0] <= ar2[0]:
+            ar3.append(ar1.pop(0))
+        else:
+            ar3.append(ar2.pop(0))
+    ar3.extend(ar1 or ar2)
+    return ar3
+    # while len(ar1) != 0 and len(ar2) != 0:
+    #     if ar1[0] > ar2[0]:
+    #         ar3.append(ar2[0])
+    #         ar2.remove(ar2[0])
+    #     else:
+    #         ar3.append(ar1[0])
+    #         ar1.remove(ar1[0])
+    #
+    # while len(ar1) > 0:
+    #     ar3.append(ar1[0])
+    #     ar1.remove(ar1[0])
+    #
+    # while len(ar2) > 0:
+    #     ar3.append(ar2[0])
+    #     ar2.remove(ar2[0])
+    #
+    # return ar3
+
+
+print(mergesort(get_list_from_user()))
